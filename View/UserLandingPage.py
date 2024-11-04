e32bfbcf6984f1718d9248a378b88a4aeb360e00
lorenzozulli <zullilorenzo.24@gmail.com>
2024-11-04 16:19:28 +0100

    Fatti rettangoli, ciao matteo

--- View/UserLandingPage.py
+++ View/UserLandingPage.py
@@ -8,13 +8,13 @@ from View.LineEdit import LineEdit
 
 pygame.init()
 
-SCREEN_WIDTH = 1360
-SCREEN_HEIGHT = 768
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
 
 # game window
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("SPECTRAVIA TCG SIM")
-# Quando avro' un icona pygame.display.set_icon("")
+# Quando avro' un icona: pygame.display.set_icon("")
 bg_image = pygame.image.load("Assets/background.jpg")
 bg_image = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -30,13 +30,13 @@ deckEditor_img = pygame.image.load("Assets/Other/deck_editor.png").convert_alpha
 settings_img = pygame.image.load("Assets/Other/settings.png").convert_alpha()
 back_img = pygame.image.load("Assets/Other/back.png").convert_alpha()
 
-multiplayer_btn = Button(128, 128, multiplayer_img, 1)
-deckEditor_btn = Button(128, 234, deckEditor_img, 1)
-settings_btn = Button(128, 340, settings_img, 1)
-back_btn = Button(128, 550, back_img, 1)
+multiplayer_btn = Button(100, 128, multiplayer_img, 1)
+deckEditor_btn = Button(100, 234, deckEditor_img, 1)
+settings_btn = Button(100, 340, settings_img, 1)
+back_btn = Button(100, 550, back_img, 1)
 
 # load lineEdit
-nameEdit = LineEdit(128, 550, 396, 40)
+nameEdit = LineEdit(100, 550, 396, 40)
 
 # load spectravia logo
 game_logo = pygame.image.load("Assets/Other/spectravia_text.png")
@@ -48,7 +48,7 @@ def randomizeCharacter():
     fileNumber = len([f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))])
     r = random.randint(1, fileNumber)
     character_img = pygame.image.load("Assets/Other/Characters/character_" + str(r) + ".jpg")
-    character_img = pygame.transform.scale(character_img, (500, 500))
+    character_img = pygame.transform.scale(character_img, (400, 400))
     return character_img
 
 character = randomizeCharacter()
@@ -89,6 +89,13 @@ def gameLoop():
                     menuState = "main"
             case "deckEditor":
                 draw_text("deck editor", font, TEXT_COL, 160, 250)
+
+                rect1 = pygame.Rect((460, 20, 800, 300))
+                rect2 = pygame.Rect((460, 340, 800, 300))
+                
+                pygame.draw.rect(screen, (255, 128, 55, 128), rect1)
+
+                pygame.draw.rect(screen, (255, 128, 55, 128), rect2)
 
                 if back_btn.draw(screen):
                     menuState = "main"
